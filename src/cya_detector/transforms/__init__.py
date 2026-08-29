c7f22dfe0de4d345ad673a88726578970e8c975b
raetan2023 <tanszelui@gmail.com>
2026-08-30 01:54:27 +0800

    feat: add isolated safe training policy

--- src/cya_detector/transforms/__init__.py
+++ src/cya_detector/transforms/__init__.py
@@ -14,16 +14,26 @@ from cya_detector.transforms.controlled import (
     apply_training_view,
     build_controlled_epoch,
 )
+from cya_detector.transforms.safe import (
+    SafePolicyError,
+    SafeSettings,
+    apply_safe,
+    validate_training_policy,
+)
 
 __all__ = [
+    "SafePolicyError",
+    "SafeSettings",
     "TrainingView",
     "TransformCell",
     "TransformContractError",
     "TransformResult",
     "apply_benchmark",
+    "apply_safe",
     "apply_training_view",
     "benchmark_cells",
     "build_controlled_epoch",
     "derive_seed",
     "validate_parent_record",
+    "validate_training_policy",
 ]
